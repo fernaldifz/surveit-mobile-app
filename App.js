057f1bd22d9a0d1f6ba92296928232dba6e36236
fernaldifz <fernaldifz@gmail.com>
2022-04-06 11:09:00 +0700

    delete theme

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import React from "react";
-import { ThemeProvider } from "@rneui/themed";
 import { Text, View } from "react-native";
-import surveitTheme from "./Theme";
 
 const HelloWorldApp = () => {
   return (
-    <ThemeProvider theme={surveitTheme}>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>Hello, world!</Text>
-      </View>
-    </ThemeProvider>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello, world!</Text>
+    </View>
   );
 };
 export default HelloWorldApp;
