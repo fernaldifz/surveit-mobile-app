20b6af0704d5956b5cd18c60dc78ae429c636a75
Jacelyn Felisha <jacelynnfelisha@gmail.com>
2022-04-22 00:41:38 +0700

    create guide card

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
-import React from "react";
-import SignUp from "./app/screens/SignUp";
-import LogIn from "./app/screens/LogIn";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
+import SignUp from './app/screens/SignUp';
+import LogIn from './app/screens/LogIn';
+import Home from './app/screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="LogIn"
-          component={LogIn}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Home"
+					component={Home}
+					options={{ headerShown: false }}
+				/>
+				<Stack.Screen
+					name="SignUp"
+					component={SignUp}
+					options={{ headerShown: false }}
+				/>
+				<Stack.Screen
+					name="LogIn"
+					component={LogIn}
+					options={{ headerShown: false }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default App;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import surveitLogo from '../assets/surveit-home.png';
-import { StyleSheet, Button, TextInput, View, Image, Text } from 'react-native';
+import guideBG from '../assets/guide-bg.png';
+import {
+	StyleSheet,
+	Button,
+	TextInput,
+	View,
+	Image,
+	Text,
+	ImageBackground,
+} from 'react-native';
 
 const Home = ({ navigation }) => {
 	return (
 		<View style={{ alignItems: 'center' }}>
 			<Image style={styles.image} source={surveitLogo} />
+			<View
+				style={{
+					alignItems: 'center',
+					borderRadius: 20,
+					overflow: 'hidden',
+					marginTop: 20,
+				}}
+			>
+				<ImageBackground style={styles.cardImage} source={guideBG}>
+					<View style={styles.seeGuideButton}>
+						<Button color="#6E61E8" title="Lihat panduan" />
+					</View>
+				</ImageBackground>
+			</View>
 		</View>
 	);
 };
@@ -22,6 +45,20 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		width: 89,
 		height: 32,
+		resizeMode: 'contain',
+	},
+	seeGuideButton: {
+		width: 140,
+		position: 'absolute',
+		top: 88,
+		left: 20,
+		borderRadius: 12,
+		overflow: 'hidden',
+		backgroundColor: '#ffffff',
+	},
+	cardImage: {
+		width: 335,
+		height: 148,
 		resizeMode: 'contain',
 	},
 });
