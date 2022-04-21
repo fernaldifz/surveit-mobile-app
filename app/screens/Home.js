37e67a986c1d5272f8bb05b75edf5dc51d597bc1
Jacelyn Felisha <jacelynnfelisha@gmail.com>
2022-04-21 22:43:47 +0700

    update home assets

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import surveitLogo from '../assets/surveit-home.png';
+import { StyleSheet, Button, TextInput, View, Image, Text } from 'react-native';
 
-const Home = () => {
-	return <div>Home</div>;
+const Home = ({ navigation }) => {
+	return (
+		<View style={{ alignItems: 'center' }}>
+			<Image style={styles.image} source={surveitLogo} />
+		</View>
+	);
 };
+
+const styles = StyleSheet.create({
+	h1: {
+		fontSize: 24,
+		lineHeight: 28,
+		marginTop: 54,
+		marginBottom: 36,
+	},
+	image: {
+		marginTop: 44,
+		alignItems: 'center',
+		width: 89,
+		height: 32,
+		resizeMode: 'contain',
+	},
+});
 
 export default Home;
