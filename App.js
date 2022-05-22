224905c4faa59eff86aae267a0237f2369c8a04d
fernaldifz <fernaldifz@gmail.com>
2022-05-22 16:25:45 +0700

    revert app.js

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
-import React from "react";
-import SignUp from "./app/screens/SignUp.js";
-import LogIn from "./app/screens/LogIn.js";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import AppLoading from "expo-app-loading";
 import {
   useFonts,
@@ -11,6 +7,11 @@ import {
   Urbanist_600SemiBold,
   Urbanist_700Bold,
 } from "@expo-google-fonts/urbanist";
+import { StackNav } from "./app/navigation";
+
+import cheveronLeft from "@assets/cheveron-left.png";
+import { Image, TouchableOpacity } from "react-native";
+import { MenuProvider } from "react-native-popup-menu";
 
 const Stack = createNativeStackNavigator();
 
@@ -26,26 +27,48 @@ const App = () => {
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          contentStyle: {
-            backgroundColor: "#FFFFFF",
-          },
-        }}
-      >
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="LogIn"
-          component={LogIn}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MenuProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={({ navigation }) => ({
+            headerTitleAlign: "center",
+            headerLeft: () => (
+              <TouchableOpacity
+                style={{ padding: 10 }}
+                onPress={() => navigation.goBack()}
+              >
+                <Image
+                  source={cheveronLeft}
+                  style={{ width: 24, height: 24 }}
+                />
+              </TouchableOpacity>
+            ),
+            headerTitleStyle: {
+              fontFamily: "Urbanist_600SemiBold",
+              fontSize: 16,
+              lineHeight: 20,
+              color: "#475569",
+            },
+            headerStyle: {
+              height: 56,
+            },
+            headerShadowVisible: false,
+          })}
+        >
+          {StackNav &&
+            StackNav.map((item, index) => {
+              return (
+                <Stack.Screen
+                  key={index}
+                  name={item.name}
+                  component={item.component}
+                  options={item.options}
+                />
+              );
+            })}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MenuProvider>
   );
 };
 
