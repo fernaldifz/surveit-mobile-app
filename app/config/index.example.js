4aebc68936630ba7871c178348d0565ed264300f
fernaldifz <fernaldifz@gmail.com>
2022-06-13 13:33:04 +0700

    update configuration

--- app/config/index.example.js
+++ app/config/index.example.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 export const app = initializeApp({
-  apiKey: "AIzaSyB0rT58CpuZZpfLYaoyu-omkJvGV2fhO0g",
-  authDomain: "surveit-app-128ab.firebaseapp.com",
-  projectId: "surveit-app-128ab",
-  storageBucket: "surveit-app-128ab.appspot.com",
-  messagingSenderId: "840663871741",
-  appId: "1:840663871741:web:b1b30c6a90ddb55dbf0e94",
-  measurementId: "G-6LLSD2MWVL",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 });
 
 export const db = getFirestore(app);
